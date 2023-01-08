6c5e3e8a19b657e5f3a15c4df03b2e9bb7bb5451
RAFAEL VALDEZ <manrvaldez.92@gmail.com>
2023-01-08 10:43:57 -0600

    rendering clear button

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import {ExpenseItem}  from './ExpenseItem'
+import { MdDelete } from 'react-icons/md';
 
-export const ExpenseList = () => {
+export const ExpenseList = ({expenses}) => {
+    console.log(expenses)
   return (
-    <div>ExpenseList</div>
+    <>
+        <ul className='list'>
+        {expenses.map((expense) => {
+            return <ExpenseItem key={expense.id} expense={expense} />
+        })}
+        </ul>
+        {expenses.length > 0 ? (
+            <button className='btn'>
+            clear expenses
+            <MdDelete className='btn-icon' />
+        </button>
+        ) : ""}
+    </>
   )
 }
