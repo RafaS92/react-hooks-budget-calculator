dd0f8ab1157985c124bd2fac20a3cbca585e57a5
RAFAEL VALDEZ <manrvaldez.92@gmail.com>
2023-01-08 10:20:31 -0600

    set up

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,34 +1,7 @@
-import React from "react";
-import { MdEdit, MdDelete } from "react-icons/md";
-const ExpenseItem = ({
-  expense: { id, charge, amount },
-  handleDelete,
-  handleEdit
-}) => {
-  return (
-    <li className="item">
-      <div className="info">
-        <span className="expense">{charge}</span>
-        <span className="amount">${amount}</span>
-      </div>
-      <div>
-        <button
-          className="edit-btn"
-          aria-label="edit button"
-          onClick={() => handleEdit(id)}
-        >
-          <MdEdit />
-        </button>
-        <button
-          className="clear-btn"
-          aria-label="delete button"
-          onClick={() => handleDelete(id)}
-        >
-          <MdDelete />
-        </button>
-      </div>
-    </li>
-  );
-};
+import React from 'react'
 
-export default ExpenseItem;
+export const ExpenseItem = () => {
+  return (
+    <div>ExpenseItem</div>
+  )
+}
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,29 +1,7 @@
-import React from "react";
-import ExpenseItem from "./ExpenseItem";
-import { MdDelete } from "react-icons/md";
-const ExpenseList = ({ expenses, handleDelete, handleEdit, clearItems }) => {
-  return (
-    <>
-      <ul className="list">
-        {expenses.map(expense => {
-          return (
-            <ExpenseItem
-              key={expense.id}
-              expense={expense}
-              handleDelete={handleDelete}
-              handleEdit={handleEdit}
-            />
-          );
-        })}
-      </ul>
-      {expenses.length > 0 && (
-        <button className="btn" onClick={clearItems}>
-          clear expenses
-          <MdDelete className="btn-icon" />
-        </button>
-      )}
-    </>
-  );
-};
+import React from 'react'
 
-export default ExpenseList;
+export const ExpenseList = () => {
+  return (
+    <div>ExpenseList</div>
+  )
+}
